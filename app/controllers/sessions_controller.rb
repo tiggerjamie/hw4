3933fc305dbd40a8a0d183b6a34ca2cb1762e4b5
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-03-16 03:52:18 +0000

    session setup and login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
-
-  def destroy
+      #find the user by their email
+     @user = User.find_by({ "email" => params["email"] })
+      #verify password
+     if @user != nil
+      #successful login
+       if BCrypt::Password.new(@user["password"]) == params["password"]
+         session["user_id"] = @user["id"]
+         flash["notice"] = "Welcome, #{@user["first_name"]}."
+         redirect_to "/places"
+       else
+      #unsuccessful pw
+         flash["notice"] = "Uh-uh honey. Not today."
+         redirect_to "/login"
+       end
+     else
+      #unsuccessful user email
+       flash["notice"] = "Uh-uh honey. Not today."
+       redirect_to "/login"
+     end
+   end
+ 
+   def destroy
+     # logout the user
+     flash["notice"] = "Until next time, dear."
+     session["user_id"] = nil
+     redirect_to "/login"
   end
 end
   
