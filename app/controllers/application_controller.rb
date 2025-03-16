19d7f9e11545a1bd9e5462425352268e6c0e8fb7
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-03-16 04:14:56 +0000

    login logout and name on navbar rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
+
+
 end
